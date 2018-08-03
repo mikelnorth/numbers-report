30fed5d4c06972ccc07adbf09efced4b92f0d98b
mikel <mikel@iltech.us>
2018-08-02 20:55:08 -0600

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from "react";
+import { Switch, Route } from "react-router-dom";
 import './App.css';
+
+//routes
+import Landing from './components/landing/Landing.js';
+import Form from './components/athlete/Form.js';
+import NotFound from './404.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route component={Landing} exact path='/' />
+          <Route component={Form} exact  path='/form' />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
